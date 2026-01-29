03275df80b41971cefaf52270a66877942a1a0ec
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2026-01-29 12:39:17 +0530

    Dynamic Component

--- src/app/registration-form-component/registration-form-component.ts
+++ src/app/registration-form-component/registration-form-component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SharedService } from '../shared-service';
 import { Router, RouterOutlet } from '@angular/router';
+import { UserInfoComponent } from '../user-info-component/user-info-component';
 
 @Component({
   selector: 'app-registration-form-component',
@@ -10,6 +11,10 @@ import { Router, RouterOutlet } from '@angular/router';
   styleUrl: './registration-form-component.scss',
 })
 export class RegistrationFormComponent implements OnInit{
+
+  @ViewChild('dynamicHost', {read: ViewContainerRef})
+  dynamicHost! : ViewContainerRef;
+
   userRegistrationForm: FormGroup = new FormGroup({});
   newUserName: string = '';
   isSubmitted = false;
@@ -36,9 +41,12 @@ export class RegistrationFormComponent implements OnInit{
   }
 
   loadUserInfo() {
+    this.dynamicHost.clear();
+    const componentRef = this.dynamicHost.createComponent(UserInfoComponent);
+    componentRef.instance.userDetails = this.userRegistrationForm.value;
 
   }
-  
+
   canDeactivate(){
     if(!this.isSubmitted){
       return confirm("'You have unsaved changes. Are you sure you want to leave this page?'")
--- src/app/user-info-component/user-info-component.ts
+++ src/app/user-info-component/user-info-component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SharedService } from '../shared-service';
 
 @Component({
@@ -9,7 +9,8 @@ import { SharedService } from '../shared-service';
 })
 export class UserInfoComponent implements OnInit {
   constructor(private ss: SharedService) {}
-
+  @Input() userDetails: any;
+  
   ngOnInit() {
     this.ss.userInfo.subscribe((res) => {
       console.log("res",res);
